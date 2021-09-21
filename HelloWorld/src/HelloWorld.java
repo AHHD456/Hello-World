ef11a78ac3d82998be032bc2c96b47eed63f8d4d
alber <alber@DESKTOP-VC1PQOL>
2021-09-21 15:19:36 +0200

    fertig

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,8 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1");
-
+		
 		System.out.print("HelloWorld, i#m a pro");
 	}
 }
