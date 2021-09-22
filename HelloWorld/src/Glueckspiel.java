16ed5b9d1470e8b5e656a10bddaf6f143677bb7e
alber <alber@DESKTOP-VC1PQOL>
2021-09-22 09:14:52 +0200

    AbfrageKonsole

--- HelloWorld/src/Glueckspiel.java
+++ HelloWorld/src/Glueckspiel.java
@@ -1,25 +1,36 @@
+import java.util.Scanner;
 
 public class Glueckspiel {
 
 	static byte alter = 19;
-	static byte geschlecht = 0;
+	static byte geschlechtz = 0;
 	static String name = "Null";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if (alter < 18 || geschlecht == 2) {
+		Scanner eingabe = new Scanner(System.in);
+		System.out.println("Wie alt bist du?");
+		alter = eingabe.nextByte();
+		if (alter > 120) {
+			System.out.println("Error");
+		}
+		System.out.println("Welches Geschlecht bist du?");
+		System.out.println("0=maennlich, 1=weiblich, 2=divers");
+		geschlechtz = eingabe.nextByte();
+		
+		
+		if (alter < 18 || geschlechtz == 2) {
 			System.out.println("Hallo " + name + " Mindestalter oder Geschlecht nicht erfüllt");
 
 		} else {
-			if (geschlecht == 1) {
+			if (geschlechtz == 1) {
 				System.out.println("Guten Tag Frau " + name + "!");
 			} else {
-				if (geschlecht == 0) {
+				if (geschlechtz == 0) {
 					System.out.println("Guten Tag Herr " + name + "!");
 				}
-			
-			
-			
+
+				
 			}
 		}
 	}
