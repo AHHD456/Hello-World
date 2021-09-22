264fd0696b3875e3bef0788b63078ad4be0c1a3c
alber <alber@DESKTOP-VC1PQOL.grube>
2021-09-22 17:34:41 +0200

    abfragd fertig

--- HelloWorld/src/Glueckspiel.java
+++ HelloWorld/src/Glueckspiel.java
@@ -5,6 +5,7 @@ public class Glueckspiel {
 	static byte alter = 19;
 	static byte geschlechtz = 0;
 	static String name = "Null";
+	static boolean entscheidung = false;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -17,21 +18,26 @@ public class Glueckspiel {
 		System.out.println("Welches Geschlecht bist du?");
 		System.out.println("0=maennlich, 1=weiblich, 2=divers");
 		geschlechtz = eingabe.nextByte();
-		
-		
+
 		if (alter < 18 || geschlechtz == 2) {
 			System.out.println("Hallo " + name + " Mindestalter oder Geschlecht nicht erfüllt");
 
 		} else {
 			if (geschlechtz == 1) {
-				System.out.println("Guten Tag Frau " + name + "!");
+				System.out.println("Guten Tag Frau " + name + "! Wollen sie spielen?");
+				entscheidung = eingabe.nextBoolean();
 			} else {
 				if (geschlechtz == 0) {
-					System.out.println("Guten Tag Herr " + name + "!");
+					System.out.println("Guten Tag Herr " + name + "! Wollen sie spielen?");
+					entscheidung = eingabe.nextBoolean();
+				} }
+            if(entscheidung == true) {
+                    	 System.out.println("true");
+                   }else {
+                    	 System.out.println("false");
+                     }
 				}
 
-				
-			}
+			
 		}
-	}
 }
