806a812dc78f050233af3e1071072282ac144e3b
alber <alber@DESKTOP-VC1PQOL.grube>
2021-09-22 20:44:43 +0200

    Name fehlt

--- HelloWorld/src/Glueckspiel.java
+++ HelloWorld/src/Glueckspiel.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Glueckspiel {
-
+	static double randomValue = 2;
 	static byte alter = 19;
 	static byte geschlechtz = 0;
 	static String name = "Null";
@@ -10,19 +11,25 @@ public class Glueckspiel {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner eingabe = new Scanner(System.in);
+		Random rand = new Random();
+
 		System.out.println("Wie alt bist du?");
 		alter = eingabe.nextByte();
 		if (alter > 120) {
 			System.out.println("Error");
-		}
+		}else {
 		System.out.println("Welches Geschlecht bist du?");
 		System.out.println("0=maennlich, 1=weiblich, 2=divers");
 		geschlechtz = eingabe.nextByte();
-
+		System.out.println("Wie ist dein Name?");
+	    
+	
 		if (alter < 18 || geschlechtz == 2) {
-			System.out.println("Hallo " + name + " Mindestalter oder Geschlecht nicht erf�llt");
+			System.out.println("Hallo " + name + " Mindestalter oder Geschlecht nicht erfüllt");
 
 		} else {
+			
+			
 			if (geschlechtz == 1) {
 				System.out.println("Guten Tag Frau " + name + "! Wollen sie spielen?");
 				entscheidung = eingabe.nextBoolean();
@@ -30,14 +37,27 @@ public class Glueckspiel {
 				if (geschlechtz == 0) {
 					System.out.println("Guten Tag Herr " + name + "! Wollen sie spielen?");
 					entscheidung = eingabe.nextBoolean();
-				} }
-            if(entscheidung == true) {
-                    	 System.out.println("true");
-                   }else {
-                    	 System.out.println("false");
-                     }
+				}
+			}
+			if (entscheidung == true) {
+			
+				randomValue = Math.random();
+				randomValue =randomValue*100;
+
+				if (randomValue < 43) {
+					System.out.println("Sie haben gewonnen. Herzlichen Glückwunsch");
+					System.out.println(randomValue);
+				} else {
+					if (randomValue > 42) {
+						System.out.println("Sie haben verloren. Versuchen sie ihr Glück nochmal");
+						System.out.println(randomValue);
+					}
 				}
 
-			
+			} else {
+				System.out.println("Bis bald, see you later alligator");
+			}
 		}
-}
+
+	}
+}}
